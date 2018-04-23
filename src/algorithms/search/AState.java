34864123bacd9bc420b398631ebe51e7eb4d95c6
guys79 <guys797@gmail.com>
2018-04-23 20:21:18 +0300

    :)

--- src/algorithms/search/AState.java
+++ src/algorithms/search/AState.java
@@ -1,4 +1,19 @@
 package algorithms.search;
 
-public class AState {
+import java.util.List;
+
+/**
+ * This class represents a state in a puzzle
+ */
+public abstract class AState {
+
+
+    private int priority;
+    private List<AState> PossibleNextStates;
+    
+
+    private AState()
+    {
+
+    }
 }
