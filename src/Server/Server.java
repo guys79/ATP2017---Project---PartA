115ecab8201b3f0576f24c89c2e030f7c76bcc51
guys79 <guys797@gmail.com>
2018-05-30 16:31:21 +0300

    V5 - With threadPool?

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -1,23 +1,56 @@
 package Server;
 
-
+import java.util.concurrent.ExecutorService;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 
+class RunMaze implements Runnable{
+
+    private Socket clientSocket;
+    private IServerStrategy serverStrategy;
+    public RunMaze(Socket s,IServerStrategy is)
+    {
+        clientSocket=s;
+        serverStrategy=is;
+    }
+
+    private void handleClient(Socket clientSocket) {
+        try {
+
+            serverStrategy.serverStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
+            clientSocket.getInputStream().close();
+            clientSocket.getOutputStream().close();
+            clientSocket.close();
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void run(){
+        handleClient(clientSocket);
+    }
+}
 
 public class Server {
     private int port;
     private int listeningInterval;
     private IServerStrategy serverStrategy;
     private volatile boolean stop;
+    private ThreadPoolExecutor executorservice;
 
 
     public Server(int port, int listeningInterval, IServerStrategy serverStrategy) {
         this.port = port;
         this.listeningInterval = listeningInterval;
         this.serverStrategy = serverStrategy;
+        int threadpoolSize=Runtime.getRuntime().availableProcessors()*2;
+        ExecutorService executorService=Executors.newCachedThreadPool();
+        this.executorservice= (ThreadPoolExecutor)executorService;
+        this.executorservice.setCorePoolSize(threadpoolSize);
     }
 
     public void start() {
@@ -33,14 +66,9 @@ public class Server {
 
             while (!stop) {
                 try {
+                    Socket clientSocket=server.accept();
 
-                    Socket clientSocket = server.accept(); // blocking call
-
-
-
-                    new Thread(() -> {
-                        handleClient(clientSocket);
-                    }).start();
+                    this.executorservice.execute(new RunMaze(clientSocket,this.serverStrategy));
                 } catch (SocketTimeoutException e) {
 
                 }
@@ -52,17 +80,7 @@ public class Server {
         }
     }
 
-    private void handleClient(Socket clientSocket) {
-        try {
 
-            serverStrategy.serverStrategy(clientSocket.getInputStream(), clientSocket.getOutputStream());
-            clientSocket.getInputStream().close();
-            clientSocket.getOutputStream().close();
-            clientSocket.close();
-        } catch (IOException e) {
-
-        }
-    }
 
     public void stop() {
 
